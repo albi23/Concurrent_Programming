e40001e62f424cf93f2b7f80d27e24b65488ed2f
albi23 <piekielny7891>
2019-04-26 21:13:03 +0200

    Added guards and stateful gorutines

--- Company_Go/Company.go
+++ Company_Go/Company.go
@@ -15,8 +15,26 @@ type task struct {
 	operation string
 }
 
-var listOfTaskToDo = make(chan task, config.TaskSize)
-var warehouse = make(chan string, config.WarehouseSize)
+type writeTask struct {
+	task task
+	resp chan bool
+}
+
+type readTask struct {
+	request bool
+	resp    chan task
+}
+
+type writeWarehouse struct {
+	result   chan bool
+	doneTask string
+}
+
+var listOfTasks = make([]task, config.TaskSize)
+var writeTaskChan = make(chan *writeTask)
+var readTaskChan = make(chan *readTask)
+var warehouseList = make([]string, 0)
+var writeWarehouseChan = make(chan *writeWarehouse)
 var deceptive = false
 
 func bossTask() {
@@ -27,8 +45,14 @@ func bossTask() {
 		firstArg := r1.Intn(10000)
 		secondArg := r1.Intn(10000)
 		operation := operations[r1.Intn(3)]
-		listOfTaskToDo <- task{firstArg, secondArg, operation}
-		if deceptive{fmt.Println("Boss add new task: ", firstArg, operation, secondArg)}
+		toDoTask := &writeTask{
+			task: task{firstArg, secondArg, operation},
+			resp: make(chan bool)}
+		writeTaskChan <- toDoTask
+		response := <-toDoTask.resp
+		if deceptive && response {
+			fmt.Println("Boss add new task: ", firstArg, operation, secondArg)
+		}
 		time.Sleep(time.Duration(config.TimeForNewTask) * time.Second)
 
 	}
@@ -37,20 +61,37 @@ func bossTask() {
 func worker(idWorker int) {
 
 	for {
-		task := <-listOfTaskToDo
-		if deceptive{fmt.Println("[Worker ", idWorker, "] Operation to do : ", task.firstArg, task.operation, task.secondArg)}
+		takenTask := &readTask{request: true, resp: make(chan task)}
+		readTaskChan <- takenTask
+		task := <-takenTask.resp
+		if deceptive && task.firstArg != 0 {
+			fmt.Println("[Worker ", idWorker, "] Operation to do : ", task.firstArg, task.operation, task.secondArg)
+		}
 		switch task.operation {
-		case "+":{
-				warehouse <- strconv.Itoa(task.firstArg + task.secondArg)
+
+		case "+":
+			{
+				newElement := &writeWarehouse{result: make(chan bool), doneTask: strconv.Itoa(task.firstArg + task.secondArg)}
+				fmt.Println("Jestem tu 1")
+
+				writeWarehouseChan <- newElement
+				<-newElement.result
+				fmt.Println("Jestem tu 3")
+
+				fmt.Println("Jestem tu4")
 				time.Sleep(time.Duration(config.TimeReciveTaskForWorker) * time.Second)
 			}
-		case "-":{
-				warehouse <- strconv.Itoa(task.firstArg - task.secondArg)
+		case "-":
+			{
+				newElement := &writeWarehouse{result: make(chan bool), doneTask: strconv.Itoa(task.firstArg - task.secondArg)}
+				writeWarehouseChan <- newElement
 				time.Sleep(time.Duration(config.TimeReciveTaskForWorker) * time.Second)
 			}
 
-		case "*":{
-				warehouse <- strconv.Itoa(task.firstArg * task.secondArg)
+		case "*":
+			{
+				newElement := &writeWarehouse{result: make(chan bool), doneTask: strconv.Itoa(task.firstArg * task.secondArg)}
+				writeWarehouseChan <- newElement
 				time.Sleep(time.Duration(config.TimeReciveTaskForWorker) * time.Second)
 			}
 
@@ -61,41 +102,84 @@ func worker(idWorker int) {
 func client() {
 
 	for {
-		productToBuy := <-warehouse
-		if deceptive {fmt.Println("Client bought product: ", productToBuy)}
+		productToBuy := &writeWarehouse{result: make(chan bool), doneTask: "aa"}
+		writeWarehouseChan <- productToBuy
+		if deceptive {
+			fmt.Println("Client bought product: ", <-productToBuy.result)
+		}
 		time.Sleep(time.Duration(config.TimeForBuy) * time.Second)
 
 	}
 }
 
-
-func checkStatusOfWarehouse()  {
-
-	var presentsAmountOfElements = len(warehouse)
-	fmt.Println("Warehouse items: ", presentsAmountOfElements)
-	counter := 0
-	for {
-		value, _ := <-warehouse
-		warehouse<-value;
-		if counter == presentsAmountOfElements {break}
-		counter++
-		fmt.Println("  ", value)
-	}
-
+func checkStatusOfWarehouse() {
+	fmt.Println(warehouseList)
 }
 
 func checkTaskToDo() {
-	var presentsAmountOfElements = len(listOfTaskToDo)
-	fmt.Println("Tasks to do: ", presentsAmountOfElements)
-	counter := 0
+	fmt.Println("Tasks to do: ", listOfTasks)
+}
+
+func taskController() {
+
 	for {
-		value, _ := <-listOfTaskToDo
-		listOfTaskToDo <-value
-		if counter == presentsAmountOfElements {break}
-		counter++
-		fmt.Println("  ", value)
+		select {
+
+		case newtask := <-taskAddGuard(len(listOfTasks) < config.TaskSize, writeTaskChan):
+			listOfTasks = append(listOfTasks, newtask.task)
+			newtask.resp <- true
+
+		case takenTask := <-taskGetGuard(len(listOfTasks) > 0, readTaskChan):
+			takenTask.resp <- listOfTasks[0]
+			listOfTasks = listOfTasks[1:]
+		}
+
 	}
 }
+
+func warehouseController() {
+
+	for {
+		select {
+		case newElement := <-warehouseGuard(len(warehouseList) < config.WarehouseSize, writeWarehouseChan):
+			warehouseList = append(warehouseList, newElement.doneTask)
+			newElement.result <- true
+			fmt.Println("warehouse controler jestem tu")
+
+			//case takeElement := <-warehouseGuard(len(warehouseList) >= 0 , writeWarehouseChan) :
+			//	takeElement.result <- warehouseList[0]
+			//	warehouseList = warehouseList[1:]
+
+		}
+	}
+}
+
+func warehouseGuard(b bool, c chan *writeWarehouse) chan *writeWarehouse {
+	fmt.Println("writeWarehouse guard, len warehouse = ", len(warehouseList), "   elements: ", warehouseList)
+	if !b {
+		fmt.Println("Fałsz ")
+
+		return nil
+	}
+	return c
+}
+
+func taskAddGuard(b bool, c chan *writeTask) chan *writeTask {
+
+	if !b {
+		return nil
+	}
+	return c
+}
+
+func taskGetGuard(b bool, c chan *readTask) chan *readTask {
+
+	if !b {
+		return nil
+	}
+	return c
+}
+
 func main() {
 
 	fmt.Println("Choose: deceptive mode/quiet mode  D/Q: ")
@@ -103,14 +187,16 @@ func main() {
 	var chosedMode string
 	fmt.Scanln(&chosedMode)
 
+	go taskController()
+	go warehouseController()
 	go bossTask()
-	for i := 0; i < config.NumberOfWorkers; i++ {go worker(i + 1)}
-	go client()
+	for i := 0; i < config.NumberOfWorkers; i++ {
+		go worker(i + 1)
+	}
+	//go client()
 
 	if chosedMode == "D" {
 		deceptive = true
-		fmt.Scanln()
-
 	} else if chosedMode == "Q" {
 
 		for {
@@ -132,4 +218,7 @@ func main() {
 		}
 
 	}
+
+	fmt.Scanln()
+
 }
